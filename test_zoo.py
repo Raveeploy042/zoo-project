b5e03f7094db37974f72a9a84b6ccebfeb9907cf
Raveeploy042 <raveeploy.char@gmail.com>
2024-11-11 05:36:50 +0000

    commited change

--- test_zoo.py
+++ test_zoo.py
@@ -7,8 +7,57 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+
     # Add your additional test cases here.
 
+    #Equivalent Class Partitioning
+
+    def test_invalid_age(self):  #class invalid
+        self.assertEqual(self.zoo.get_ticket_price(-1), 'Invalid Age')
+
+    def test_child_price(self): #class 0-12
+        self.assertEqual(self.zoo.get_ticket_price(6), 50)
+
+    def test_teenager_price(self): #class 13-20
+        self.assertEqual(self.zoo.get_ticket_price(18), 100)
+
+    def test_worker_price(self): #class 21-60
+        self.assertEqual(self.zoo.get_ticket_price(42), 150)
+    
+    def test_older_price(self): #class higher 60 (> 60)
+        self.assertEqual(self.zoo.get_ticket_price(70), 100)
+
+    #Boundary Value Analysis 
+
+    #class invalid
+    def test_invalid_age2(self):  
+        self.assertEqual(self.zoo.get_ticket_price(-1), 'Invalid Age')
+
+    #class 0-12
+    def test_child_price1(self): 
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    def test_child_price2(self): 
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    #class 13-20
+    def test_teenager_price1(self): 
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+
+    def test_teenager_price2(self): 
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    #class 21-60
+    def test_worker_price1(self): 
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_worker_price2(self): 
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    
+    #class higher 60 (> 60)
+    def test_older_price1(self): 
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
+           
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12: #add from 0 < age to 0 <= age
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20: #add from age < 20 to age <= 20
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: #add from 21 < age to 21 <= age
             return 150
         elif age >= 60:
             return 100
+        else :  #add case: lower than 0 => Invalid age
+            return 'Invalid Age'
